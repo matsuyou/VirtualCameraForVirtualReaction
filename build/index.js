50c28d30bc6fd829af7e246fd3bc98f07760f137
matsuyou <mtk4091@yahoo.co.jp>
2022-05-25 00:01:13 +0900

    jsファイル追加、名称変更

--- build/index.js
+++ build/index.js
@@ -2,8 +2,15 @@
 window.open('http://werid-air.main.jp/work/', '', 'top=0,left=0,width=800,height=600');
 var script = document.createElement("script");
 script.setAttribute("type", "module");
-script.setAttribute("src", chrome.runtime.getURL("main.js"));
+script.setAttribute("src", chrome.runtime.getURL("camera.js"));
+var script2 = document.createElement("script");
+script2.setAttribute("type", "module");
+script2.setAttribute("src", chrome.runtime.getURL("gui.js"));
 var head = document.head ||
     document.getElementsByTagName("head")[0] ||
     document.documentElement;
 head.insertBefore(script, head.lastChild);
+head.insertBefore(script2, head.lastChild);
+var body = document.body;
+var html = 'test';
+body.insertAdjacentHTML('afterbegin', html);
--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,13 @@
-
+//仮想モデルページを新規ウィンドウで開く(仮)
 window.open('http://werid-air.main.jp/work/', '', 'top=0,left=0,width=800,height=600');
+
 //jsファイルを指定したscriptタグを追加
 const script = document.createElement("script");
 script.setAttribute("type", "module");
-script.setAttribute("src", chrome.runtime.getURL("main.js"));
+script.setAttribute("src", chrome.runtime.getURL("camera.js"));
+const script2 = document.createElement("script");
+script2.setAttribute("type", "module");
+script2.setAttribute("src", chrome.runtime.getURL("gui.js"));
 
 // サイトの `<head />` を取得する
 const head =
@@ -13,5 +17,11 @@ const head =
 
 // `<script />` を挿入する
 head.insertBefore(script, head.lastChild);
+head.insertBefore(script2, head.lastChild);
 
+// サイトの `<body />` を取得する
+const body = document.body
+const html = 'test'
 
+//  `<body />`の先頭に挿入する。
+body.insertAdjacentHTML('afterbegin', html);
