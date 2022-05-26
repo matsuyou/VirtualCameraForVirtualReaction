a355b85516c8c282bea762b93e0bde779abf2ba9
matsuyou <mtk4091@yahoo.co.jp>
2022-05-26 09:00:39 +0900

    cuiテスト追加

--- build/index.js
+++ build/index.js
@@ -5,11 +5,16 @@ script.setAttribute("src", chrome.runtime.getURL("camera.js"));
 var script2 = document.createElement("script");
 script2.setAttribute("type", "module");
 script2.setAttribute("src", chrome.runtime.getURL("gui.js"));
+var script3 = document.createElement("link");
+script3.setAttribute("rel", "stylesheet");
+script3.setAttribute("type", "text/css");
+script3.setAttribute("href", chrome.runtime.getURL("gui.css"));
 var head = document.head ||
     document.getElementsByTagName("head")[0] ||
     document.documentElement;
 head.insertBefore(script, head.lastChild);
 head.insertBefore(script2, head.lastChild);
+head.insertBefore(script3, head.lastChild);
 var body = document.body;
 var html = '<input type="button" value="REACTION START" id="ReactionStart">\
 <input type="button" value="😄" id="Reaction1">\
--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,14 @@
-//jsファイルを指定したscriptタグを追加
+//js,cssファイルを指定したscriptタグ、linkタグを追加
 const script = document.createElement("script");
 script.setAttribute("type", "module");
 script.setAttribute("src", chrome.runtime.getURL("camera.js"));
 const script2 = document.createElement("script");
 script2.setAttribute("type", "module");
 script2.setAttribute("src", chrome.runtime.getURL("gui.js"));
+const script3 = document.createElement("link");
+script3.setAttribute("rel", "stylesheet");
+script3.setAttribute("type", "text/css");
+script3.setAttribute("href", chrome.runtime.getURL("gui.css"));
 
 // サイトの `<head />` を取得する
 const head =
@@ -12,9 +16,10 @@ const head =
   document.getElementsByTagName("head")[0] ||
   document.documentElement;
 
-// `<script />` を挿入する
+//タグを挿入する
 head.insertBefore(script, head.lastChild);
 head.insertBefore(script2, head.lastChild);
+head.insertBefore(script3, head.lastChild);
 
 // サイトの `<body />` を取得する
 const body = document.body
