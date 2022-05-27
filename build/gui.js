2ff6bfbfd8606abf6e7f523b9467a9c54774d0ad
matsuyou <mtk4091@yahoo.co.jp>
2022-05-27 11:59:02 +0900

    別ブラウザ操作テスト

--- build/gui.js
+++ build/gui.js
@@ -1,20 +1,27 @@
 "use strict";
+var win1 = null;
 function WindowOpen() {
-    window.open('http://werid-air.main.jp/work/', '', 'top=0,left=0,width=800,height=600');
+    if (win1 == null) {
+        win1 = window.open('http://werid-air.main.jp/work/', '', 'top=0,left=0,width=800,height=600');
+    }
+}
+function WindowClose() {
+    win1.close();
+    win1 = null;
 }
 var button = document.getElementById('ReactionStart');
 if (button !== null)
     button.onclick = WindowOpen;
 var button1 = document.getElementById('Reaction1');
 if (button1 !== null)
-    button1.onclick = WindowOpen;
+    button1.onclick = WindowClose;
 var button2 = document.getElementById('Reaction2');
 if (button2 !== null)
-    button2.onclick = WindowOpen;
+    button2.onclick = WindowClose;
 var button3 = document.getElementById('Reaction3');
 if (button3 !== null)
-    button3.onclick = WindowOpen;
+    button3.onclick = WindowClose;
 var button4 = document.getElementById('Reaction4');
 if (button4 !== null)
-    button4.onclick = WindowOpen;
+    button4.onclick = WindowClose;
 console.log("gui");
--- src/gui.ts
+++ src/gui.ts
@@ -1,21 +1,30 @@
+var win1: any = null;
+
 //仮想モデルページを新規ウィンドウで開く(仮)
 function WindowOpen() {
-    window.open('http://werid-air.main.jp/work/', '', 'top=0,left=0,width=800,height=600');
+    if (win1 == null){
+        win1 = window.open('http://werid-air.main.jp/work/', '', 'top=0,left=0,width=800,height=600');
+    }
+}
+
+function WindowClose() {
+    win1.close();
+    win1 = null;
 }
 
 let button = document.getElementById('ReactionStart');
 if (button !== null) button.onclick = WindowOpen;
 
 let button1 = document.getElementById('Reaction1');
-if (button1 !== null) button1.onclick = WindowOpen;  
+if (button1 !== null) button1.onclick = WindowClose;  
 
 let button2 = document.getElementById('Reaction2');
-if (button2 !== null) button2.onclick = WindowOpen;  
+if (button2 !== null) button2.onclick = WindowClose;  
 
 let button3 = document.getElementById('Reaction3');
-if (button3 !== null) button3.onclick = WindowOpen;  
+if (button3 !== null) button3.onclick = WindowClose;  
 
 let button4 = document.getElementById('Reaction4');
-if (button4 !== null) button4.onclick = WindowOpen;  
+if (button4 !== null) button4.onclick = WindowClose;  
 
 console.log("gui");
