860118b4f3862fcae7cc6ed2d6b2d49b90d25cde
matsuyou <mtk4091@yahoo.co.jp>
2022-06-12 22:45:03 +0900

    リンク微修正

--- build/gui.js
+++ build/gui.js
@@ -2,7 +2,7 @@
 var win = window;
 var subWin = null;
 function WindowOpen() {
-    subWin = window.open('http://localhost:8000/controller/', 'sample', 'top=50,left=50,width=600,height=100');
+    subWin = window.open('http://localhost:8000/gui/', 'sample', 'top=50,left=50,width=600,height=100');
 }
 var start_button = document.getElementById('ReactionStart');
 if (start_button != null)
