60a95de084e6ee194b5ac6b98f8d12d8fe0797d8
matsuyou <mtk4091@yahoo.co.jp>
2022-07-18 00:24:40 +0900

    マニフェスト修正

--- build/gui.js
+++ build/gui.js
@@ -2,7 +2,7 @@
 var win = window;
 var subWin = null;
 function WindowOpen() {
-    subWin = window.open('https://glacial-meadow-27403.herokuapp.com/gui/', 'sample', 'top=50,left=50,width=1125,height=215');
+    subWin = window.open('https://glacial-meadow-27403.herokuapp.com/gui/', 'sample', 'top=50,left=50,width=900,height=200');
 }
 var start_button = document.getElementById('ReactionStart');
 if (start_button != null)
--- src/gui.ts
+++ src/gui.ts
@@ -5,7 +5,7 @@ var subWin: any = null;
  * @description 仮想モデルページを新規ウィンドウで開く/閉じる(仮)
  */
 function WindowOpen() {
-    subWin = window.open('https://glacial-meadow-27403.herokuapp.com/gui/', 'sample', 'top=50,left=50,width=1125,height=215');
+    subWin = window.open('https://glacial-meadow-27403.herokuapp.com/gui/', 'sample', 'top=50,left=50,width=900,height=200');
 }
 /**
  * @description ボタン押下時の処理設定
