c071124ea1df908ce1d475d484cf56bb2b7046c4
matsuyou <mtk4091@yahoo.co.jp>
2023-01-01 23:47:56 +0900

    GUIページのURLをHerokuから修正

--- build/gui.js
+++ build/gui.js
@@ -2,7 +2,7 @@
 var win = window;
 var subWin = null;
 function WindowOpen() {
-    subWin = window.open('https://glacial-meadow-27403.herokuapp.com/gui/', 'sample', 'top=50,left=50,width=900,height=200');
+    subWin = window.open('https://werid-air.main.jp/work/VirtualReaction/gui/', 'sample', 'top=50,left=50,width=900,height=200');
 }
 var start_button = document.getElementById('ReactionStart');
 if (start_button != null)
--- src/gui.ts
+++ src/gui.ts
@@ -5,7 +5,8 @@ var subWin: any = null;
  * @description 仮想モデルページを新規ウィンドウで開く/閉じる(仮)
  */
 function WindowOpen() {
-    subWin = window.open('https://glacial-meadow-27403.herokuapp.com/gui/', 'sample', 'top=50,left=50,width=900,height=200');
+    /*subWin = window.open('https://glacial-meadow-27403.herokuapp.com/gui/', 'sample', 'top=50,left=50,width=900,height=200');*/
+    subWin = window.open('https://werid-air.main.jp/work/VirtualReaction/gui/', 'sample', 'top=50,left=50,width=900,height=200');
 }
 /**
  * @description ボタン押下時の処理設定
